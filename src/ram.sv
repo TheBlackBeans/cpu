0fd0bbbdcacf034031fa828d9eb4c15ca1bbc791
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-12-06 18:17:39 +0100

    [ram] Made iverilog accept the module.

--- src/ram.sv
+++ src/ram.sv
@@ -1,6 +1,8 @@
 `default_nettype none
 
-module Ram #(parameter addr_size = 4, parameter cell_size = 16)
+module Ram #(parameter addr_size = 4,
+	     parameter cell_size = 16,
+	     parameter size = 2**addr_size)
    (input logic clk,
     input logic			 reset_n,
     input logic [addr_size-1:0]	 ra,
@@ -9,11 +11,12 @@ module Ram #(parameter addr_size = 4, parameter cell_size = 16)
     input logic			 we,
     output logic [cell_size-1:0] result);
    
-   logic [cell_size-1:0]	 ram [2**addr_size] = '{ default: 0 };
+   logic [cell_size-1:0]	 ram [size];
    
    always @(posedge clk or negedge reset_n) begin
       if (!reset_n)
-	ram = '{ default: 0 };
+	for (int i = 0; i < size; i++)
+	  ram[i] <= 0;
       else begin
 	 result <= ram[ra];
 	 if (we)
