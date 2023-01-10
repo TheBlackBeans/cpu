b9a0d6c43414182e91c95741e60247bf45bff03a
rajdakin <rajdakin@gmail.com>
2023-01-10 19:25:11 +0100

    [WRAPPER] Updated the Python wrapper for better I/O and timing

--- src/wrappers/clock.py
+++ src/wrappers/clock.py
@@ -1,18 +1,13 @@
 from subprocess import Popen, PIPE, STDOUT
 from threading import Thread
 from time import sleep
+import datetime
 import urwid, urwid.curses_display
 
 class State:
     def __init__(self):
-        self._a = False
-        self._z = False
-    @property
-    def a(self):
-        return "1" if self._a else "0"
-    @property
-    def z(self):
-        return "1" if self._z else "0"
+        self.a = False
+        self.z = False
 
 class Wrapper(Thread):
     def __init__(self, state, label, loop):
@@ -30,19 +25,23 @@ class Wrapper(Thread):
         ) as f:
             if f.stdout is None or f.stdin is None:
                 exit(1)
+            target = datetime.datetime.now()
             while not self.stop:
                 line = f.stdout.readline().decode("utf-8").strip()
                 if f.poll() is not None:
                     break
                 if line == "WAIT":
-                    sleep(0.00045)
+                    target += datetime.timedelta(microseconds=500)
+                    delay = (target - datetime.datetime.now()).total_seconds()
+                    if delay > 0:
+                        sleep(delay)
                     f.stdin.write("\n".encode())
                     f.stdin.flush()
                 elif line == "0>":
-                    f.stdin.write((self._state.a + "\n").encode())
+                    f.stdin.write((("1" if self._state.a else "0") + "\n").encode())
                     f.stdin.flush()
                 elif line == "1>":
-                    f.stdin.write((self._state.z + "\n").encode())
+                    f.stdin.write((("1" if self._state.z else "0") + "\n").encode())
                     f.stdin.flush()
                 else:
                     self._label.set_text(line)
@@ -50,17 +49,17 @@ class Wrapper(Thread):
 
 state = State()
 label = urwid.Text("bonjour")
-boutons = urwid.Text("0 0")
+boutons = urwid.Text("    ")
 
 def update_state(key):
     if key in {'a', 'A'}:
-        state._a = not state._a
+        state.a = not state.a
     elif key in {'z', 'Z'}:
-        state._z = not state._z
+        state.z = not state.z
     elif key in {'q', 'Q'}:
         wrapper.stop = True
         raise urwid.ExitMainLoop()
-    boutons.set_text(f"{state.a} {state.z}")
+    boutons.set_text(("#" if state.a else " ") + "  " + ("#" if state.z else " "))
 
 pile = urwid.Pile([label, boutons])
 top = urwid.Filler(pile, valign='top')
